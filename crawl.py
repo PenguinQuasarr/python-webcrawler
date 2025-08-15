0dca6f6d86b7ebebde2d6d79e0d861fd7731af1c
PenguinQuasarr <james201501@protonmail.com>
2025-08-15 08:29:58 +0100

    Prototype crawl working

--- crawl.py
+++ crawl.py
@@ -1,6 +1,7 @@
 from urllib.parse import urlparse
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup
+import requests
 
 
 def normalize_url(input_url):
@@ -19,9 +20,43 @@ def get_urls_form_html(html, base_url):
     list_of_urls = []
 
     for link in soup_obj.find_all("a"):
-        print(link)
         url = link.get("href")
         absolute_url = urljoin(base_url, url)
         list_of_urls.append(absolute_url)
 
     return list_of_urls
+
+
+def get_html(url):
+    request_obj = requests.get(url)
+    content_type = request_obj.headers["content-type"]
+
+    if not request_obj.ok or "text/html" not in content_type:
+        return "Nope"
+    else:
+        return request_obj.text
+
+
+def crawl_page(base_url, current_url=None, pages=None):
+
+    if base_url not in current_url:
+        return
+
+    current_normalized = normalize_url(current_url)
+
+    if current_normalized not in pages:
+        pages[current_normalized] = 1
+    else:
+        pages[current_normalized] += 1
+        return
+
+    if ".xml" in current_url:
+        return
+
+    html_page = get_html(current_url)
+    print(f"Getting page: {current_url}")
+
+    urls_list = get_urls_form_html(html_page, base_url)
+
+    for url in urls_list:
+        crawl_page(base_url, url, pages)
--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import sys
+from crawl import crawl_page
+
+
 def main():
-    print("Hello from webcrawler!")
+
+    if len(sys.argv) < 2:
+        print("no website provided")
+        sys.exit(1)
+    if len(sys.argv) > 2:
+        print("too many arguments provided")
+        sys.exit(1)
+
+    print(f"starting crawl of: {sys.argv[1]}")
+    url = sys.argv[1]
+    page = {}
+    crawl_page(url, url, page)
+    print(page)
 
 
 if __name__ == "__main__":
